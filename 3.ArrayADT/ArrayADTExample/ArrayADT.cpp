e18f60b5abedd67612d8f0fb5c4d13100fa7b3e5
Marcos Velásquez <marcosv200007@gmail.com>
2025-05-26 17:16:43 -0600

    Finishing binary search

--- 3.ArrayADT/ArrayADTExample/ArrayADT.cpp
+++ 3.ArrayADT/ArrayADTExample/ArrayADT.cpp
@@ -72,6 +72,25 @@ int LinearSearch(struct Array *arr, int key)
     return -1;
 }
 
+int BinarySearch(struct Array arr, int key)
+{
+    int l, mid, h;
+    l = 0;
+    h = arr.length - 1;
+
+    while (l <= h) 
+    {
+        mid = (l + h) / 2;
+        if (key == arr.A[mid])
+            return mid;
+        else if (key < arr.A[mid])
+            h = mid - 1;
+        else
+            l = mid + 1;
+    }
+    return -1;
+}
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
@@ -79,8 +98,8 @@ int main()
     //Append(&arr, 10);
     //Insert(&arr, 5, 10);
     //printf("%d\n", Delete(&arr, 4));
-
-    printf("%d\n", LinearSearch(&arr, 5));
+    //printf("%d\n", LinearSearch(&arr, 5));
+    printf("%d\n", BinarySearch(arr, 10));
     Display(arr);
 
 
