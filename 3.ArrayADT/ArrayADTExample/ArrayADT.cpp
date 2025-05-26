9932bc1d780946bb47ed16dfd767bed90e23ac31
Marcos Velásquez <marcosv200007@gmail.com>
2025-05-26 16:45:55 -0600

    Adding linear search function.

--- 3.ArrayADT/ArrayADTExample/ArrayADT.cpp
+++ 3.ArrayADT/ArrayADTExample/ArrayADT.cpp
@@ -51,13 +51,36 @@ int Delete(struct Array *arr, int index)
     return 0;
 }
 
+void Swap(int *x, int *y) 
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int LinearSearch(struct Array *arr, int key)
+{
+    for (int i = 0; i < arr->length; i++)
+    {
+        if (key == arr->A[i])
+        {
+            Swap(&arr->A[i], &arr->A[0]);
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
 
     //Append(&arr, 10);
     //Insert(&arr, 5, 10);
-    printf("%d\n", Delete(&arr, 4));
+    //printf("%d\n", Delete(&arr, 4));
+
+    printf("%d\n", LinearSearch(&arr, 5));
     Display(arr);
 
 
