c2e0a56bd3006016f0581cddafba120986b36eb6
Marcos Velásquez <marcosv200007@gmail.com>
2025-06-05 12:21:55 -0600

    Adding common operations in Array

--- 3.ArrayADT/ArrayADTExample/ArrayADT.cpp
+++ 3.ArrayADT/ArrayADTExample/ArrayADT.cpp
@@ -155,9 +155,74 @@ void Reverse2(struct Array *arr)
         Swap(&arr->A[i], &arr->A[j]);
 }
 
+void InsertSort(struct Array *arr, int x)
+{
+    if (arr->length == arr->size)
+        return;
+    int i = arr->length - 1;
+    while(i >= 0 && arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i + 1] = x;
+    arr->length++;
+}
+
+int isSorted(struct Array arr)
+{
+    int i;
+    for (i = 0; i < arr.length - 1; i++)
+        if(arr.A[i] > arr.A[i+1])
+            return 0;
+    return 1;
+}
+
+void ReArrange(struct Array *arr)
+{
+    int i, j;
+    i = 0;
+    j = arr->length - 1;
+
+    while (i < j)
+    {
+        while (arr->A[i] < 0) i++;
+        while (arr->A[j] >= 0) j--;
+        if (i < j) Swap(&arr->A[i], &arr->A[j]);
+    }
+}
+
+struct Array Merge(){
+    struct Array result;
+}
+
+struct Array* Merge(struct Array *arr1, struct Array *arr2)
+{
+    int i, j, k;
+    i=j=k=0;
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+    for (; i < arr1->A[i]; i++)
+        arr3->A[k++] = arr1->A[i];
+    for (; j < arr2->A[i]; j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+
+    return arr3;
+}
+
 int main()
 {
-    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
+    //struct Array arr = {{2, -3, 25, 10, -15, -7}, 10, 6};
 
     //Append(&arr, 10);
     //Insert(&arr, 5, 10);
@@ -165,8 +230,17 @@ int main()
     //printf("%d\n", LinearSearch(&arr, 5));
     //printf("%d\n", BinarySearch(arr, 10));
     //printf("%f\n", Avg(arr));
-    Reverse2(&arr);
-    Display(arr);
+    //Reverse2(&arr);
+    //InsertSort(&arr, 1);
+    //printf("%d \n", isSorted(arr));
+    //ReArrange(&arr);
+    //Display(arr);
+
+    struct Array arr1 = {{2,6,10,15,25}, 10, 5};
+    struct Array arr2 = {{3,4,7,18,20}, 10, 5};
+    struct Array *arr3 = Merge(&arr1, &arr2);
+
+    Display(*arr3);
 
     printf("\n\n\n\n\n\n\n\n\n\n");
     return 0;
